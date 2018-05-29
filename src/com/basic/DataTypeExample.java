b0679da9642ca218bcef426e918717ad12f2504c
PElanchezhiyan <elanche97@gmail.com>
2018-05-29 15:32:09 +0530

    basic name program using eclipse software

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main (String[] args) {
+		String firstName = "Elanchezhiyan";
+		String secondName = "Poosamani";
+		System.out.println(firstName + " " +secondName);
+	}
+
+}
