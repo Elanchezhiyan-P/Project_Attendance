f378ea7b612fffc50df3c940f4c035342769c0cd
PElanchezhiyan <elanche97@gmail.com>
2018-05-29 15:50:23 +0530

    Addition program using eclipse software

---
+++ src/com/basic/Addition.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class Addition {
+	public static void main (String[] args) {
+	byte one = 20;
+	byte two = 40;
+	int three = one + two ;
+	System.out.println(three);
+	}
+}
