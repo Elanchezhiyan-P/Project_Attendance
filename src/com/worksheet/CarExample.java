bdceb8bbbf0d1f20cf546efa406b402077f210c8
PElanchezhiyan <elanche97@gmail.com>
2018-06-03 16:22:07 +0530

    simple CarExample program using eclipse software

--- src/com/worksheet/CarExample.java
+++ src/com/worksheet/CarExample.java
@@ -14,7 +14,7 @@ public static void main (String[] args)
 	Car car = new Car();
 	
 	car.setSpeed(100);
-	CarExample carexample = new CarExample();
+	CarExample CarExample = new CarExample();
 	
 	CarExample.aMethod(car);
 	System.out.println("speed " + car.getSpeed());
